31b4d27d7132a38bead0078b1cc61123a319f89b
Gwen Duncan <gldunc01@gmail.com>
2022-05-25 14:46:38 -0400

    Added knowledge check requirements

--- KnowledgeCheck1GDuncan.py
+++ KnowledgeCheck1GDuncan.py
@@ -1,2 +1,21 @@
-print('Testing')
-print('It worked!')
+print('Hello World!')
+
+
+clients = [
+    {
+    "name" : "John",
+    "age" : 12,
+    "gender" : "male",
+    "city_state" : ("Phoenix", "Arizona")},
+    {
+    "name" : "Kim",
+    "age" : 39,
+    "gender" : "female",
+    "city_state" : ("Seattle", "Washington")
+    }
+]
+
+print('Our new client is', clients[1])
+
+print('Coming all the way from', clients[1]["city_state"])
+
