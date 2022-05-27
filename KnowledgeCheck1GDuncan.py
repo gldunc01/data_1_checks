14095cf9a952608c30ef7b124f2d05e91b3c86aa
Gwen Duncan <gldunc01@gmail.com>
2022-05-26 20:13:33 -0400

    added comments and formatting

--- KnowledgeCheck1GDuncan.py
+++ KnowledgeCheck1GDuncan.py
@@ -1,6 +1,9 @@
+from unicodedata import name
+
+# statement that prints Hello World
 print('Hello World!')
 
-
+# a list with several values and a dictionary with 2 key value pairs and a tuple with 4 values
 clients = [
     {
     "name" : "John",
@@ -15,7 +18,12 @@ clients = [
     }
 ]
 
-print('Our new client is', clients[1])
+# print one of the dictionary items and one of the tuples
+# added formatting which is why you see so many prints
 
-print('Coming all the way from', clients[1]["city_state"])
+print('Our new client is:\n', "", clients[1]["name"])
+print(" ", clients[1]["age"])
+print(" ", clients[1]["gender"])
+
+print(" ", 'Coming all the way from', clients[1]["city_state"][0], clients[1]["city_state"][1])
 
