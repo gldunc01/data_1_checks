21e6b69354aa4c0680b228bd6e4eb66a8a2592e5
Gwen Duncan <gldunc01@gmail.com>
2022-06-13 20:16:08 -0400

    Changed plot to pull from excel file instead of list

--- kc_2.py
+++ kc_2.py
@@ -4,20 +4,14 @@ import numpy as np
 
 GA_data = pd.read_excel(r'C:\Users\gduncan\OneDrive - Metro United Way, Inc. (191100)\Desktop\Professional Development\Python\Code_Louisville\data_1_checks\assets\GeneralAssembly.xlsx')
 
-GA_data_by_career = GA_data.groupby('Which career path did you choose with General Assembly?')['Respondent ID'].nunique()
+GA_data_by_career = GA_data.groupby('Which career path did you choose with General Assembly?', as_index=False)['Respondent ID'].nunique()
 
 print(GA_data_by_career)
 
-career = ['Data Analytics', 'Digital Marketing', 'Front End Web Dev', 'UX Design']
-num_of_students = [58, 14, 10, 21]
-
-
 plt.style.use('dark_background')
 plt.figure(figsize = (12,6))
-
-plt.bar(career, num_of_students)
+plt.bar(GA_data_by_career['Which career path did you choose with General Assembly?'], GA_data_by_career['Respondent ID'])
 plt.xlabel('Career Type')
-plt.ylabel('Number of Students')
+plt.ylabel('Number of Graduates')
 plt.title('Careers Chosen by Graduates')
-
 plt.show()
